88d496bde4bd3e0224849169c20f781ab92ef84d
Jacek Sprzączkowski <yatzek@yatzek.pl>
2019-06-24 23:05:41 +0200

    js playerMove parameters

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ else if (randomNumber == '3'){
     computerMove = 'nożyce';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+}
+else if (playerInput == '2'){
+  playerMove = 'papier';
+}
+else if (playerInput == '3'){
+  playerMove = 'papier';
+}
+else (playerInput > '3')
+{
+  printMessage('Błąd:wybierz liczbę 1, 2 lub 3');
+}
