ab32a731cfcef3f6ab9cf838ef20f4737459331d
Jacek Sprzączkowski <yatzek@yatzek.pl>
2019-06-24 22:51:15 +0200

    js computerMovee parameters

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+}
+else if (randomNumber == '2'){
+    computerMove = 'papier';
+}
+else if (randomNumber == '3'){
+    computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
